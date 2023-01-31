27d423961aabc79ca85e60e2aeba70b9b6ab7673
feisa ramadhani <111243780+feisa11@users.noreply.github.com>
2023-01-31 09:43:11 +0700

    Add files via upload

---
+++ JS Bilangan Prima/prima.js
@@ -0,0 +1,19 @@
+const apakahPrima = (angka) => {
+    let pembagi = 0
+    for (let i=1; i <= angka; i++) {
+        if (angka % i == 0) {
+            pembagi++
+        }
+    }
+    if (pembagi == 2) {
+        console.log("prima")
+    } else {
+        console.log("bukan prima")
+    }
+}
+
+// TEST CASES
+apakahPrima(); //prima
+apakahPrima(); //prima
+apakahPrima(); //bukan prima
+apakahPrima(); //bukan prima
